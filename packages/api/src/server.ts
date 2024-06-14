0c119f3f1f8f3afeb255c86490070ddea79a285c
Pedro Guerrato <pguerrato@gmail.com>
2024-06-14 20:23:56 -0300

    Add User and Post schema

---
+++ packages/api/src/server.ts
@@ -0,0 +1,38 @@
+import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
+import 'dotenv/config'
+
+const useLogger: boolean = ['local', 'development'].includes(process.env.NODE_ENV ?? '') ? true : false
+
+const fastify: FastifyInstance = Fastify({ logger: useLogger })
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: (process.env.PORT as number | undefined) || 3000, ipv6Only: false })
+    const address = fastify.server.address()
+
+    if (!address) {
+      throw new Error('Server address is not available.')
+    }
+
+    if (process.env.NODE_ENV !== 'development') {
+      const port = typeof address === 'string' ? address : address?.port
+      console.log(`Server listening at ${port}`)
+    } else {
+      if (typeof address === 'string') {
+        console.log(`Server listening at ${address}`)
+      } else {
+        let addressMessage = `\n\n\x1b[32mServer listening at\x1b[0m\n`
+        for (const addr of fastify.addresses()) {
+          addressMessage += `\t - http://${addr.address}:${addr.port}\n`
+        }
+
+        console.log(addressMessage)
+      }
+    }
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
